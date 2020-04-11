4b760efe2f29af534f71896aabed6dcf93c08dac
Scott Small <smallsco@gmail.com>
2020-04-11 16:03:49 -0700

    3.1 phonebook backend step1

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+
+let phonebook = [
+  {
+    "name": "John Barron",
+    "number": "123-555-4567",
+    "id": 1
+  },
+  {
+    "name": "John Miller",
+    "number": "321-555-7654",
+    "id": 2
+  },
+  {
+    "name": "Carolin Gallego",
+    "number": "213-555-5467",
+    "id": 3
+  },
+  {
+    "name": "David Dennison",
+    "number": "231-555-6754",
+    "id": 4
+  },
+]
+
+app.get('/api/persons', (req, res) => {
+  res.json(phonebook)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
