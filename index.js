c28586278ec4f159293e4a01f7a1ef7150d4f165
Scott Small <smallsco@gmail.com>
2020-04-11 17:20:37 -0700

    3.7 phonebook backend step7

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 /* Dependencies */
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 /* Middleware */
+app.use(morgan('tiny'))
 app.use(express.json())
 
 /* "Database" */
