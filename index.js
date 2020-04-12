7999746a857801e0c28dbf55b9c04034c10bdc1e
Scott Small <smallsco@gmail.com>
2020-04-11 17:32:07 -0700

    3.8 phonebook backend step8

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ const morgan = require('morgan')
 const app = express()
 
 /* Middleware */
-app.use(morgan('tiny'))
+app.use(morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    req.method === 'POST' ? JSON.stringify(req.body) : ''
+  ].join(' ')
+}))
 app.use(express.json())
 
 /* "Database" */
